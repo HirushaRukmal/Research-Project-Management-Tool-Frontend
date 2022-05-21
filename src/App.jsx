f49ee36a4a3e0721ee433385ac023916f6d43ea3
Isuru Pathum Herath <77333662+isurupathumherath@users.noreply.github.com>
2022-05-21 19:59:31 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
                 <Routes>
 
                     {/* Public Route */}
-                    <Route path="/" element={<Home />} />
+                    <Route path="/home" element={<Home />} />
                 </Routes>
             </Router>
         </>
