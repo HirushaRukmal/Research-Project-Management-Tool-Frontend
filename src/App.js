99cc826f52b91ae4b2c795922b973f0388065fca
Isuru Pathum Herath <77333662+isurupathumherath@users.noreply.github.com>
2022-05-21 20:28:30 +0530

    build issue fixed with parcel build and run

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
                 <Routes>
 
                     {/* Public Route */}
-                    <Route path="/home" element={<Home />} />
+                    <Route path="/" element={<Home />} />
                 </Routes>
             </Router>
         </>
