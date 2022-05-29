edc339f4b3c26ed5be353c8c1cc12c2d4695bb50
samuditha jayawardena <54886813+samuditha97@users.noreply.github.com>
2022-05-30 01:47:51 +0530

    create admin panel

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Register from './pages/student/register';
 import AdminRegister from './pages/admin/register';
 import AdminDashboard from './pages/admin/dashboard';
 import AdminLogin from './pages/admin/login';
+import SideMenu from './components/Admin/sideMenu';
+import AddSubTypes from './components/Admin/add-subTypes';
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           <Route path="/admin/register" element={<AdminRegister />} />
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
           <Route path="/admin/login" element={<AdminLogin />} />
+          <Route path="/admin/home" element={<SideMenu/>}/>
+          <Route path='/admin/subTypes' element={<AddSubTypes/>}/>
         </Routes>
       </Router>
     </>
