3a6350753bd7c03bfa178324994f4b8de45b382e
HirushaRukmal <hirusharukmal266@gmail.com>
2022-06-02 21:06:38 +0530

    updates to submission types

--- src/components/Admin/list_subTypes.js
+++ src/components/Admin/list_subTypes.js
@@ -1,26 +1,31 @@
 import React, { Component } from "react";
-import axios from 'axios';
-import Table from 'react-bootstrap/Table';
-import SubTypeTableRow from './SubTypeTableRow';
+import axios from "axios";
+import Table from "react-bootstrap/Table";
+import SubTypeTableRow from "./SubTypeTableRow";
+import Navbar from "../../pages/admin/Navbar";
+import Sidebar from "../../pages/admin/Sidebar";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+import ReactHTMLTableToExcel from "react-html-table-to-excel";
 
 export default class subTypeList extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-        admin_sts: [],
+      admin_sts: [],
     };
-    
   }
   componentDidMount() {
-    axios.get('http://localhost:8000/admin/')
-      .then(res => {
+    axios
+      .get("http://localhost:8000/admin/")
+      .then((res) => {
         this.setState({
-            admin_sts: res.data
+          admin_sts: res.data,
         });
       })
       .catch((error) => {
         console.log(error);
-      })
+      });
   }
   DataTable() {
     return this.state.admin_sts.map((res, i) => {
@@ -29,20 +34,58 @@ export default class subTypeList extends Component {
   }
 
   render() {
-    return (<div className="table-wrapper" >
-      <Table striped bordered hover > 
-        <thead className="listTable">
-          <tr>
-            <th>Submission Topic</th>
-            <th>Deadline</th>
-            <th>Description</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.DataTable()}
-        </tbody>
-      </Table>
-    </div>);
+    return (
+      <div>
+        <Navbar />
+        <Sidebar />
+        <div className="container">
+          <h2>Submission Types</h2>
+          <br />
+          <p>
+            <button className="btn btn-primary">
+              <a href="/subTypes" className="text-decoration-none text-white">
+                Create Submission Type
+              </a>
+            </button>
+
+            <div style={{ marginTop: "-38px", marginLeft: "230px" }}>
+              <ReactHTMLTableToExcel
+                className="btn btn-outline-success"
+                table="table"
+                filename="Submission Type Details"
+                sheet="Sheet"
+                buttonText="Generate Sheet"
+              />
+            </div>
+
+            <div
+              className="float-end"
+              style={{ marginTop: "-30px", marginLeft: "190px" }}
+            >
+              <input
+                className="form-control"
+                type="search"
+                placeholder="Search here"
+                name="searchQuery"
+              ></input>
+            </div>
+          </p>
+          <Table id="table" class="table">
+            <thead>
+              <tr>
+                <th scope="col">Topic</th>
+                <th scope="col">Deadline</th>
+                <th scope="col">Description</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>{this.DataTable()}</tbody>
+          </Table>
+          <p>
+            <b>Total Submission Types: {this.state.admin_sts.length}</b>
+          </p>
+        </div>
+      </div>
+    );
   }
 }
