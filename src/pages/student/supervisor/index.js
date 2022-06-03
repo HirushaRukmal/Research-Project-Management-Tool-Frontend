90cd08700af5d40bde8213a40951bf213d7ed0fa
Isuru Pathum Herath <77333662+isurupathumherath@users.noreply.github.com>
2022-06-03 19:35:36 +0530

    Retrive supervisor and co-supervisor ids

--- src/pages/student/supervisor/index.js
+++ src/pages/student/supervisor/index.js
@@ -10,19 +10,35 @@ const App = () => {
 
     const [currentGroup, setCurrentGroup] = useState([]);
     const [dataAvailable, setDataAvailability] = useState(false);
-    
+    const [supervisorGroup, setSupervisorGroup] = useState([]);
+
 
     const fetchCurrentGroup = () => {
-        console.log("WORKING");
         axios.get(`${process.env.BACKEND_API_LOCAL}/group/groupData/${getStudentId()}`)
             .then(response => {
                 console.log(response.data);
                 setCurrentGroup(response.data);
+                axios.get(`${process.env.BACKEND_API_LOCAL}/supervisor-group/getByGroupId/${response.data._id}`)
+                    .then(response => {
+                        console.log(response.data);
+                        setSupervisorGroup(response.data);
+                        console.log(supervisorGroup);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
                 console.log(currentGroup);
             })
             .catch(error => {
                 console.log(error);
             });
+        fetchSupervisorGroup();
+
+    }
+
+    const fetchSupervisorGroup = () => {
+        console.log(currentGroup._id);
+
     }
 
     const updateGroup = () => {
@@ -54,19 +70,41 @@ const App = () => {
                             <h4>Group Topic: {currentGroup.groupTopic}</h4>
                         </center>
                         <br />
+                        <div>
+                            <nav class="navbar navbar-expand-md navbar-dark " style={{ position: 'static', marginLeft: "32%" }}>
+                                <ul class="navbar-nav">
+                                    <li class="nav-item active">
+                                        <a class="nav-link" href="/request-supervisor">
+                                            <button className="btn btn-info" type="submit" style={{ color: "white" }}>Request Supervisor/Co-Supervisor</button>
+                                        </a>
+                                    </li>
+                                    {/* <li class="nav-item">
+                            <a class="nav-link">
+                                <button onClick={deleteGroup} className="btn btn-info" style={{ color: "white" }} >Delete Project Group</button>
+                            </a>
+                        </li> */}
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link">
+                                            <button onClick={updateGroup} className="btn btn-info" style={{ color: "white" }} >Re-request Supervisor/Co-Supervisor</button>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <br />
                         <center>
-                            {/* <div class="container profile-page">
+                            <div class="container profile-page">
                                 <div class="row">
                                     <div class="col-xl-6 col-lg-7 col-md-12">
                                         <div class="card profile-header">
                                             <div class="body">
                                                 <div class="row">
                                                     <div class="col">
-                                                        <h3 class="m-t-0 m-b-0"><strong>{student1.fullName}</strong></h3>
-                                                        <h4 class="job_post">{student1.sliitId}</h4>
+                                                        <h3 class="m-t-0 m-b-0"><strong>{supervisorGroup.supervisorId}</strong></h3>
+                                                        {/* <h4 class="job_post">{supervisorGroup.sliitId}</h4>
                                                         <p>SLIIT Email Address: {student1.sliitEmail}</p>
                                                         <p>Personal Email Address: {student1.personalEmail}</p>
-                                                        <p>Contact Number: {student1.contactNo}</p>
+                                                        <p>Contact Number: {student1.contactNo}</p> */}
                                                         <div>
                                                             <button class="btn btn-primary btn-round">Follow</button> &nbsp;
                                                             <button class="btn btn-primary btn-round btn-simple">Message</button>
@@ -83,11 +121,11 @@ const App = () => {
                                             <div class="body">
                                                 <div class="row">
                                                     <div class="col">
-                                                        <h3 class="m-t-0 m-b-0"><strong>{student2.fullName}</strong></h3>
-                                                        <h4 class="job_post">{student2.sliitId}</h4>
+                                                        <h3 class="m-t-0 m-b-0"><strong>{supervisorGroup.coSupervisorId}</strong></h3>
+                                                        {/* <h4 class="job_post">{student2.sliitId}</h4>
                                                         <p>SLIIT Email Address: {student2.sliitEmail}</p>
                                                         <p>Personal Email Address: {student2.personalEmail}</p>
-                                                        <p>Contact Number: {student2.contactNo}</p>
+                                                        <p>Contact Number: {student2.contactNo}</p> */}
                                                         <div>
                                                             <button class="btn btn-primary btn-round">Follow</button> &nbsp;
                                                             <button class="btn btn-primary btn-round btn-simple">Message</button>
@@ -98,7 +136,7 @@ const App = () => {
                                         </div>
                                     </div>
                                 </div>
-                            </div> */}
+                            </div>
                         </center>
                     </div>
                 ) : (
@@ -111,27 +149,7 @@ const App = () => {
 
             </div>
 
-            <div>
-                <nav class="navbar navbar-expand-md navbar-dark " style={{ position: 'static', marginLeft: "32%" }}>
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="/request-supervisor">
-                                <button className="btn btn-info" type="submit" style={{ color: "white" }}>Request Supervisor/Co-Supervisor</button>
-                            </a>
-                        </li>
-                        {/* <li class="nav-item">
-                            <a class="nav-link">
-                                <button onClick={deleteGroup} className="btn btn-info" style={{ color: "white" }} >Delete Project Group</button>
-                            </a>
-                        </li> */}
-                        <li class="nav-item dropdown">
-                            <a class="nav-link">
-                                <button onClick={updateGroup} className="btn btn-info" style={{ color: "white" }} >Re-request Supervisor/Co-Supervisor</button>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+
 
 
         </div >
