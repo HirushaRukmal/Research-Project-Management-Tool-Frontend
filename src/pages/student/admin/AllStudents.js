30e5e2effd960c1fceff1032b584b000103bd97d
Isuru Pathum Herath <77333662+isurupathumherath@users.noreply.github.com>
2022-06-03 22:11:12 +0530

    student retrive and delete implemented

--- src/pages/student/admin/AllStudents.js
+++ src/pages/student/admin/AllStudents.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 import axios from 'axios';
 import Swal from "sweetalert2";
 import ReactHTMLTableToExcel from 'react-html-table-to-excel';
+import Navbar from '../../admin/Navbar';
+import Sidebar from '../../admin/Sidebar';
+import '../../../assets/student/scrollable-div.css';
 
 const App = () => {
 
@@ -16,6 +19,21 @@ const App = () => {
                 console.log(response)
                 setStudent(response.data);
                 setCount(response.data.length);
+            })
+            .catch(error => console.log(error));
+    }
+
+    const deleteStudent = (studentId) => {
+        axios
+            .delete(`${process.env.BACKEND_API_LOCAL}/student/${studentId}`)
+            .then(response => {
+                // alert(response.data.message);
+                Swal.fire(
+                    `Studnet under Register ID ${studentId} is Deleted`,
+                    `${response.data.message}`,
+                    'success'
+                )
+                fetchStaffMembers();
             })
             .catch(error => console.log(error));
     }
@@ -37,15 +55,14 @@ const App = () => {
                         response.sliitEmail.toLowerCase().includes(searchWord.toLowerCase()) ||
                         response.personalEmail.toLowerCase().includes(searchWord.toLowerCase()) ||
                         response.contactNo.toLowerCase().includes(searchWord.toLowerCase()) ||
-                        response.studentType.toLowerCase().includes(searchWord.toLowerCase()) ||
-                        response.groupStatus.toLowerCase().includes(searchWord.toLowerCase());
+                        response.studentType.toLowerCase().includes(searchWord.toLowerCase());
                 });
 
                 if (searchWord === "") {
                     console.log("EMPLTY");
                     fetchStudent();
                 } else {
-                    fetchStudent(newFilter);
+                    setStudent(newFilter);
                 }
             })
             .catch(error => console.log(error));
@@ -53,106 +70,108 @@ const App = () => {
 
     return (
         <div>
-            <div className="card" >
-                <div className="card-body">
-                    <h1 align="center">Registred Stundets</h1>
-                    <br />
-                    <div>
-                        <center>
-                            <div
-                                className="border border-info"
-                                style={{
-                                    width: "100%",
-                                    backgroundColor: "white",
-                                    borderRadius: "10px",
-                                    borderColor: "#00408C",
-                                    padding: "20px 20px 20px 20px",
-                                    margin: "10px 0px 0px 0px",
-                                    align: "center"
-                                }}
-                            >
-                                <div className="row">
-                                    <div className="col">
-                                        <span style={{ color: "blue" }}><h3>{count}</h3></span>
-                                        <span><h3>Number of Students</h3></span>
+            <Navbar />
+            <Sidebar />
+            <div style={{ position: "absolute" }}>
+                <div className="card scrollable-div" style={{ width: "100%", height: "590px" }}>
+                    <div className="card-body">
+                        <h1 align="center">Registred Stundets</h1>
+                        <br />
+                        <div>
+                            <center>
+                                <div
+                                    className="border border-info"
+                                    style={{
+                                        width: "80%",
+                                        backgroundColor: "white",
+                                        borderRadius: "10px",
+                                        borderColor: "#00408C",
+                                        padding: "20px 20px 20px 20px",
+                                        margin: "10px 0px 0px 0px",
+                                        align: "center"
+                                    }}
+                                >
+                                    <div className="row">
+                                        <div className="col">
+                                            <span style={{ color: "blue" }}><h3>{count}</h3></span>
+                                            <span><h3>Number of Students</h3></span>
+                                        </div>
                                     </div>
-                                    {/* <div className="col">
-                                        <h3>{count}</h3>
-                                    </div> */}
                                 </div>
-                            </div>
-                            <form style={{ marginTop: '40px', marginLeft: '20px', marginRight: '40px', width: "100%" }}>
-                                <div className="col-lg-3 mt-2 mb-2">
-                                    <input
-                                        className="form-control"
-                                        type="search"
-                                        placeholder="Search"
-                                        value={wordEntered}
-                                        onChange={handleFilter}
-                                    />
-                                </div>
-                            </form>
-                        </center>
-                    </div>
+                                <form style={{ marginTop: '40px', marginLeft: '20px', marginRight: '40px', width: "100%" }}>
+                                    <div className="col-lg-3 mt-2 mb-2">
+                                        <input
+                                            className="form-control"
+                                            type="search"
+                                            placeholder="Search"
+                                            value={wordEntered}
+                                            onChange={handleFilter}
+                                        />
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <ReactHTMLTableToExcel
+                                                className='btn btn-outline-success'
+                                                table='table'
+                                                filename='Students Excel'
+                                                sheet='Sheet'
+                                                buttonText='Download Excel Sheet'
+                                            />
+                                        </div>
+                                    </div>
+                                </form>
 
+                            </center>
+                        </div>
 
-                    <table id="table" class="table" responsive className="table table-hover" style={{ marginTop: '40px', marginLeft: '20px', width: '95%' }}>
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Registered ID</th>
-                                <th>Student Name</th>
-                                <th>SLIIT ID</th>
-                                <th>SLIIT Email</th>
-                                <th>Personal Email</th>
-                                <th>Contact Number</th>
-                                <th>Stuednt Type</th>
-                                <th>Group Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {student.map((student, i) => (
-                                <tr key={i}>
-                                    <th scope="row">{i + 1}</th>
+                        <div className=" scrollable-div">
+                            <table id="table" class="table" responsive className="table table-hover" style={{ marginTop: '40px', marginLeft: '20px', width: '95%' }}>
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Registered ID</th>
+                                        <th>Student Name</th>
+                                        <th>SLIIT ID</th>
+                                        <th>SLIIT Email</th>
+                                        <th>Personal Email</th>
+                                        <th>Contact Number</th>
+                                        <th>Stuednt Type</th>
+                                        <th>Group Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {student.map((student, i) => (
+                                        <tr key={i}>
+                                            <th scope="row">{i + 1}</th>
 
-                                    <a href={`#`} style={{ textDecoration: 'none' }}>
-                                        <td>{student._id}</td>
-                                    </a>
+                                            <a href={`#`} style={{ textDecoration: 'none' }}>
+                                                <td>{student._id}</td>
+                                            </a>
 
-                                    <td>{student.fullName}</td>
-                                    <td>{student.sliitId}</td>
-                                    <td>{student.sliitEmail}</td>
-                                    <td>{student.personalEmail}</td>
-                                    <td>{student.contactNo}</td>
-                                    <td>{student.mobileNumber}</td>
-                                    <td>{student.studentType}</td>
-                                    <td>{student.groupStatus.toString()}</td>
-                                    <td>
-                                        <a className="" href={`/updateStaffMember/${student.employeeId}`}>
-                                            <i className="fas fa-edit"></i>&nbsp;
-                                        </a>
-                                        &nbsp;
-                                        <a className="" href="#" onClick={() => deleteStudent(student.employeeId)}>
-                                            <i className="far fa-trash-alt"></i>&nbsp;
-                                        </a>
-                                        &nbsp;
-                                        <a href={`/attendance/${student.employeeId}`} style={{ textDecoration: 'none' }}>
-                                            <i class="fas fa-calendar-week"></i>&nbsp;
-                                        </a>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                    <br />
-                    <div style={{ marginTop: '', marginLeft: "1030px" }}>
-                        <ReactHTMLTableToExcel
-                            className='btn btn-outline-success'
-                            table='table'
-                            filename='Students Excel'
-                            sheet='Sheet'
-                            buttonText='Download Excel Sheet'
-                        />
+                                            <td>{student.fullName}</td>
+                                            <td>{student.sliitId}</td>
+                                            <td>{student.sliitEmail}</td>
+                                            <td>{student.personalEmail}</td>
+                                            <td>{student.contactNo}</td>
+                                            <td>{student.studentType}</td>
+                                            <td>{student.groupStatus.toString()}</td>
+                                            <td>
+                                                <a className="" href={`/updateStaffMember/${student.employeeId}`}>
+                                                    <i className="fas fa-edit"></i>&nbsp;
+                                                </a>
+                                                &nbsp;&nbsp;&nbsp;
+                                                <a className="" href="#" onClick={() => deleteStudent(student._id)}>
+                                                    <i className="far fa-trash-alt"></i>&nbsp;
+                                                </a>
+
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                        <br />
+
                     </div>
                 </div>
             </div>
